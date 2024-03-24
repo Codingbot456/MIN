a8364ac77771cd77fcdc7c627f23fe1977f3b613
Codingbot456 <135627476+Codingbot456@users.noreply.github.com>
2024-03-24 17:54:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
+	spaceBetween: 80,
+	freeMode: true,
+	pagination: {
+	  el: ".swiper-pagination",
+	  clickable: true,
+	},
+  });
+
+
+
+ScrollReveal().reveal('h1', 
+{ 
+	origin : 'left',
+  delay    : 100,
+  distance : '120px',
+  easing   : 'ease-in-out',
+
+ });
+
+ ScrollReveal().reveal('h2,h3,h4', 
+{ 
+	origin : 'right',
+  delay    : 200,
+  distance : '120px',
+  easing   : 'ease-in',
+
+ });
+
+ ScrollReveal().reveal('p1,p2,p3', 
+ { 
+	delay:300,
+    scale      : 1,
+    distance   : '5px',
+	reset:'false',
+    origin     : 'left',
+    easing     : 'ease-out',
+    viewFactor : 1,
+ 
+  });
+ 
+  ScrollReveal().reveal('p1,p2,p3', 
+  { 
+   delay:300,
+     scale      : 1,
+     distance   : '5px',
+   reset:'false',
+     origin     : 'left',
+     easing     : 'ease-out',
+     viewFactor : 1,
+  
+   });
+  
+   
+    
+    $(document).ready(function(){
+      $("#stevo").click(function(){
+        $(".toggle-nav").toggle();
+      });
+    });
+       
+$(document).ready(function(){
+  $(".stevo").click(function(){
+    $(".toggle-nav").animate({left: '20px'}, "slow");
+  });
+});
+
+
+    
+    
+   
+    
+   
